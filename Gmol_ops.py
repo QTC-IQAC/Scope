dbbb8f6f0a6338e669292fd2ec5a5435ee5e6be2
Sergi Vela <g4vela@portal03>
2022-10-21 16:32:42 +0200

    Gmol operations added

---
+++ Gmol_ops.py
@@ -0,0 +1,8 @@
+
+
+def gmol_update_geom(mol: object, new_coord: list):
+    mol.old_coord = mol.coord
+    mol.coord = new_coord
+    for idx, a in enumerate(mol.atoms):
+        a.old_coord = a.coord
+        a.coord = new_coord[idx]
