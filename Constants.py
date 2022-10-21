656dc891c7831d71799d6ccbb588637dfc02a3f2
Sergi Vela <sergi.vela@gmail.com>
2022-10-21 16:28:02 +0200

    Updated Constants

--- Constants.py
+++ Constants.py
@@ -1,10 +1,16 @@
 import numpy as np
 
 boltz=0.00000316682968    ## hartree/K
+speed_light=299792458     ## meter/second
+planck_hs=1.5198300452227972e-16  ## hartree/second
+planck_Js=6.626076E-34  ## J/second
+
+#################
 
 ### Conversion Factor. Always to be applied as multiplications
 
 har2kJmol=2625.5002
+har2cm=219474.6
 eV2har=0.03674930495
 cm2har=0.000004556335281
 
