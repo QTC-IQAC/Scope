47d68615a6f3b3e30368e5bdd7fcc9c8824aab38
Sergi Vela <sergi.vela@gmail.com>
2022-10-25 16:30:44 +0200

    VNM tools

--- Read_Write.py
+++ Read_Write.py
@@ -10,3 +10,14 @@ def load_binary(pathfile):
     with open(pathfile, "rb") as pickle_file:
         binary = pickle.load(pickle_file)
     return binary
+
+def writexyz(fdir, fname, labels, pos, charge: int=0, spin: int=1):
+    if fdir[-1] != "/":
+        fdir = fdir + "/"
+    natoms = len(labels)
+    fullname = fdir + fname
+    with open(fullname, "w") as fil:
+        print(natoms, file=fil)
+        print(charge, spin, file=fil)
+        for idx, l in enumerate(labels):
+            print("%s  %.6f  %.6f  %.6f" % (l, pos[idx][0], pos[idx][1], pos[idx][2]),file=fil)
---
+++ VNM_tools.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def write_vnm_dyn(vnm: object, maxfactor: int=10, index: int=0, outfolder: str='./'):
+    filename: str="dyn_vnm_"+str(index)+".xyz"
+    with open(outfolder+filename, "w") as output_file:    
+        for f in range(-maxfactor,maxfactor,1):
+            natoms = len(vnm.xs)
+            print(natoms, file=output_file)
+            print("", file=output_file)
+            for idx in range(natoms):
+                vector = np.array([vnm.xs[idx], vnm.ys[idx], vnm.zs[idx]])
+                pos = geom[idx]+vector*f*0.1
+                label = elemdatabase.elementsym[vnm.atnums[idx]]
+                print(f"{label}  {pos[0]:.5f}  {pos[1]:.5f}  {pos[2]:.5f}", file=output_file)
+
+def vnm_displacement(VNMs: list, initial_coord: list, which: list=[], which_side: str='pos', maxfactor: int=10, debug: int=0):
+    # Applies a displacement from the initial geometry using either 'all negative normal modes whose index is in 'which'
+    if len(which) == 0: which = [vnm.index for vnm in VNMs]
+    new_coord = initial_coord.copy()    
+    if debug >= 1: print("initial coord:", new_coord[0])
+    for vnm in VNMs:
+        if vnm.freq < 0.0 and vnm.index in which:
+            if debug >= 1: print("displacing VNM:", vnm.freq_cm)
+            for idx in range(len(vnm.xs)):
+                vector = np.array([vnm.xs[idx], vnm.ys[idx], vnm.zs[idx]])
+                if which_side.lower() == 'pos': displacement = vector*maxfactor*0.1
+                elif which_side.lower() == 'neg': displacement = -vector*maxfactor*0.1
+                new_coord[idx] = new_coord[idx]+displacement
+                if idx == 0 and debug >= 1: print("displaced coord:", new_coord[idx])
+    return new_coord
