5305f79dc3964bdd0140458d44a32d9158d441e1
Sergi Vela <g4vela@portal03>
2023-01-05 16:57:14 +0100

    Run number and hierarchy added

--- __init__.py
+++ __init__.py
@@ -3,8 +3,11 @@
 ###scopepath = '/home/g4vela/SCOPE/Database_SCO/Scripts/Scope'
 ###sys.path.append(scopepath)
 
-from .Constants import *
 from .Adapted_from_cell2mol import *
+from .Constants import *
+from .Classes_QC import *
+from .Classes_Job import *
+from .Classes_SCO import *
 from .Control_Jobs import *
 from .Geom_SCO_V1 import *
 from .Other import *
@@ -14,9 +17,6 @@ from .Gmol_ops import *
 from .Parse_G16_outputs import * 
 from .Parse_QE_outputs import *
 from .Read_Write import *
-from .Classes_QC import *
-from .Classes_Job import *
-from .Classes_SCO import *
 from .Thermal_Corrections import *
 from .unit_cell_tools import *
 from .VNM_tools import *
