9a89bde62d997752a8dcc8e327e28ff13c23df6e
Sergi Vela <g4vela@portal03>
2023-02-08 16:52:03 +0100

    Gmol corrected bug

--- Gmol_ops.py
+++ Gmol_ops.py
@@ -1,13 +1,13 @@
 from cell2mol.tmcharge_common import *
 
-def gmol_update_geom(mol: object, new_coord: list):
+def gmol_update_geom(mol: object, new_coord: list, debug: int=0):
     mol.old_coord = mol.coord
     mol.coord = new_coord
     for idx, a in enumerate(mol.atoms):
         a.old_coord = a.coord
         a.coord = new_coord[idx]
 
-def cell_update_geom(cell: object, new_coord: object):
+def cell_update_geom(cell: object, new_coord: object, debug: int=0):
     assert hasattr(cell,"cellparam")
     cell.old_moleclist = cell.moleclist.deepcopy()
     cell.moleclist = moleclist.deepcopy()
