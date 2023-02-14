e5f95072ac23b54bd7bb6c8a98eb35d00537cdbf
svela-bs <sergi.vela@gmail.com>
2023-02-14 17:44:14 +0100

    minor bugs in Gmol_ops

--- Gmol_ops.py
+++ Gmol_ops.py
@@ -5,7 +5,7 @@ from Scope.VNM_tools import vnm_displacement
 def cell_postocoord(cell: object) -> None:
     if not hasattr(cell,"coord") and hasattr(cell,"pos"): setattr(cell,"coord",cell.pos)
 
-def gmol_update_geom(mol: object, name: str="coord", new_coord: list, debug: int=0) -> None:
+def gmol_update_geom(mol: object, new_coord: list, name: str="coord", debug: int=0) -> None:
     assert hasattr(mol,name)
     old_coord = getattr(mol,name)
     setattr(mol,"old_"+name,old_coord)
@@ -15,12 +15,12 @@ def gmol_update_geom(mol: object, name: str="coord", new_coord: list, debug: int
         setattr(at,"old_"+name,old_coord)
         setattr(at,name,new_coord[idx])
 
-def gmol_create_geom(mol: object, name: str="new_geom", new_coord: list, debug: int=0) -> None:
+def gmol_create_geom(mol: object, new_coord: list, name: str="new_coord", debug: int=0) -> None:
     setattr(mol,name,new_coord)
     for idx, at in enumerate(mol.atoms):
         setattr(at,name,new_coord[idx]
 
-def cell_update_geom(cell: object, moleclist: list, name: str="pos", new_coord: list, debug: int=0) -> None:
+def cell_update_geom(cell: object, moleclist: list, new_coord: list, name: str="coord", debug: int=0) -> None:
     assert hasattr(cell,"cellparam")
     assert hasattr(cell,name)
     cell.old_moleclist = cell.moleclist
@@ -40,7 +40,7 @@ def cell_update_geom(cell: object, moleclist: list, name: str="pos", new_coord: 
             setattr(at,"old_"+name,old_coord)
             setattr(at,name,new_coord[idx])
 
-def cell_create_geom(cell: object, moleclist: list, name: str="new_geom", new_coord: list, debug: int=0) -> None:
+def cell_create_geom(cell: object, moleclist: list, new_coord: list, name: str="new_coord", debug: int=0) -> None:
     assert hasattr(cell,"moleclist")        ## Makes sure it is a cell
     setattr(cell,name,new_coord)            ## Creates new attribute to object cell, with name "name" and value "new_coord"
     for idx, mol in enumerate(cell.moleclist):
