58e11df94186d6707b140fb20bee5ea02a6123c8
svela-bs <sergi.vela@gmail.com>
2025-10-21 12:29:58 +0200

    removed G16 functions call

--- src/Scope/Software/Gaussian/__init__.py
+++ src/Scope/Software/Gaussian/__init__.py
@@ -1,4 +1,3 @@
-from Scope.Software.Gaussian.G16_Functions import *
 from Scope.Software.Gaussian.G16_Output import *
 from Scope.Software.Gaussian.G16_Parse import * 
 from Scope.Software.Gaussian.G16_Input import *
